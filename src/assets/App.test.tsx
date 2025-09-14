0ddd4a4f710b854cee987f0672e4e2e685bca6a1
Serg Kulish <funnywebart@gmail.com>
2025-09-14 21:17:04 +0400

    test: fix unused describe in App.test

--- src/assets/App.test.tsx
+++ src/assets/App.test.tsx
@@ -3,7 +3,10 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-it('renders Geo Dashboard heading', () => {
-  render(<App />);
-  expect(screen.getByRole('heading', { name: /geo dashboard/i })).toBeInTheDocument();
+describe('App component', () => {
+    it('renders Geo Dashboard heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /geo dashboard/i })).toBeInTheDocument();
+    });
 });
+
